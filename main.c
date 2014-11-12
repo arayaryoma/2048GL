46255983990afd18281bcc5042cfab8c9320eda8
Ryoma ABE <arayahomuhomu@gmail.com>
2014-11-12 21:40:29 +0900

    ランダムな位置に出現

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <GL/glut.h>
 #include <stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+double x=-0.8,y=-0.8;
+int map[3][3];
+double pr_x,pr_y;
 void display( void )
 {
   glClear( GL_COLOR_BUFFER_BIT );
@@ -27,9 +33,16 @@ void display( void )
   glVertex2d(0.8,0.0);
   glVertex2d(-0.8,0.4);
   glVertex2d(0.8,0.4);
-
-
   glEnd();
+  glColor3f(0.87,0.8,0.01);
+  glBegin(GL_POLYGON);
+  glVertex2f(pr_x+0.01,pr_y+0.01);
+  glVertex2f(pr_x+0.39,pr_y+0.01);
+  glVertex2f(pr_x+0.39,pr_y+0.39);
+  glVertex2f(pr_x+0.01,pr_y+0.39);
+  glEnd();
+
+  
   glFlush();
 }
 
@@ -51,7 +64,7 @@ void resize( int w, int h )
       -x, x,
       -y, y
       );
-  printf( "%d, %d \r", w, h );
+  
 }
 
 void init( void )
@@ -59,15 +72,46 @@ void init( void )
   glClearColor( 0.0, 0.0, 0.0, 1.0 );
 }
 
+static void timer(int dummy){
+  glutTimerFunc(5,timer,0);
+  glutPostRedisplay();
+}
+
+void keys(unsigned char key ,int dummy1,int dummy2){
+  switch(key){
+    case 'k':
+      y+=0.4;
+      break;
+    case 'j':
+      y-=0.4;
+      break;
+    case 'h':
+      x-=0.4;
+      break;
+    case 'l':
+      x+=0.4;
+      break;
+  }
+
+}
+
+
+
 int main( int argc, char** argv )
 {
+  srand( (unsigned)time(NULL));
+  pr_x=-0.8+(0.4*( rand()%4) );
+  pr_y=-0.8+(0.4*( rand()%4) );
+  
   glutInit( &argc, argv );
   glutInitDisplayMode( GLUT_RGBA );
   glutInitWindowSize( 800, 800 );
   glutCreateWindow( argv[0] );
   glutDisplayFunc( display);
   glutReshapeFunc( resize );
+  timer(0);
   init();
+  glutKeyboardFunc(keys);
   glutMainLoop();
 
   return( 0 );
